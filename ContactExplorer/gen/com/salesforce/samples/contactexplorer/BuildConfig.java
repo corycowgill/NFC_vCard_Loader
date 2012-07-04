e2960db033d3b86267a60c3afe01a1c82cfea8ec
corycowgill <corycowgill@yahoo.com>
2012-07-04 16:28:41 -0500

    Initial Sync
    Initial Sync

---
+++ ContactExplorer/gen/com/salesforce/samples/contactexplorer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.salesforce.samples.contactexplorer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
